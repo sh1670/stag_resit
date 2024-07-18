4d3924add59529d492cb514527ebf491af014ad5
sh1670 <sh1670@bristol.ac.uk>
2024-07-18 16:43:45 +0100

    Update GraphParserExample.java

--- resources/code/GraphParserExample.java
+++ resources/code/GraphParserExample.java
@@ -7,6 +7,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.io.IOException;
 
 public class GraphParserExample {
 
